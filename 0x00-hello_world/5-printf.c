328dc98c4ded8edf1e55efbe3f25b1956009b3be
Edna Atieno <91233810+Misspowers1@users.noreply.github.com>
2022-12-08 19:15:10 +0300

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,7 +5,6 @@
  * but the outcome is a pice of art,
  * Return: 0 if exited properly, non-zero otherwise
  */
-
 int main(void)
 {
 	printf("with proper grammer, but the outcome is a piece of art,\n");
