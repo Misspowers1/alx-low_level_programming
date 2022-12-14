891c76a3b276d0b8f0132ce837d085f57d3d6e46
Edna Atieno <91233810+Misspowers1@users.noreply.github.com>
2022-12-14 17:35:54 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - prints _putchar, followed by a new line
+ * main -  prints _putchar, followed by a new line
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -14,7 +14,7 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
-	-putchar('\n');
+	_putchar('\n');
 
 	return (0);
 }
