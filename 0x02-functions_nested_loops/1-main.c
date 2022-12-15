47ace5311e3c26b3ae21dcb1aeafec337e202dce
Misspowers1 <misspowers01@gmail.com>
2022-12-15 03:42:23 -0800

    islower

--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -7,6 +7,14 @@
  */
 int main(void)
 {
-	print_alphabet_x10();
+	int c;
+
+	c = _islower('H');
+	_putchar(c + '0');
+	c = _islower('o');
+	_putchar(c + '0');
+	c = _islower(108);
+	_putchar(c + '0');
+	_putchar('\n');
 	return (0);
 }
