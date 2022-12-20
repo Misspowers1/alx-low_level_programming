528856d13de2f51fde5626b6c54802a66d50b21d
Misspowers1 <misspowers01@gmail.com>
2022-12-20 11:39:38 -0800

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,18 +1,9 @@
-/**
- *
- *  * reset_to_98 - takes a pointer to an int parameter
- *
- *   *               and updates the value it points to 98
- *
- *    *
- *
- *     * @n: int parameter
- *
- *      *
- *
- *       * Return: Nothing
- *
- *        */
+/*
+ *reset_to_98 - takes a pointer to an int parameter
+ *and updates the value it points to 98
+ *@n: int parameter
+ *Return: Nothing
+ */
 
 
 
